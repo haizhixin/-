6e0988cba89836780716fd6e6fcfef1b7526e824
haizhixin <1126917181@qq.com>
2021-02-25 20:26:01 +0800

    typescript

---
+++ 01-05.静态类型的深度理解.ts
@@ -0,0 +1,10 @@
+interface Point {
+  x: number;
+  y: number;
+}
+
+const point: Point = {
+  x: 3,
+  y: 4
+};
+
---
+++ 03-06.类中的泛型以及泛型类型/demo.ts
@@ -0,0 +1,36 @@
+// interface Item {
+//   name: string;
+// }
+
+
+// class DataManager<T extends number | string> {
+//   constructor(private data: T[]) {}
+//   getItem(index: number): T {
+//     return this.data[index];
+//   }
+// }
+
+// const data = new DataManager<number>([1]);
+// data.getItem(0);
+
+// const data = new DataManager([
+//   {
+//     name: 'dell'
+//   }
+// ]);
+
+// interface Test {
+//   name: string;
+// }
+
+// const data = new DataManager<number>([]);
+
+// 如何使用泛型作为一个具体的类型注解
+function hello<T>(params: T) {
+  return params;
+}
+
+const func: <T>(param: T) => T = hello;
+
+// 如何使用泛型作为一个具体的类型注解
+const func:<T>(param:T)=>T = hello
